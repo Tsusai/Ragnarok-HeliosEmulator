9065e2181c9d7fc09b2cd8a3bb91d2ad36a51b78
Tsusai <tsusai@gmail.com>
2006-05-06 20:40:32 +0000

    - Rev 3- [2006/05/06] - Tsusai - Overview : Updated Serveroptions to what RaX wants, an "OnChange" idea. [2006/05/06] - Tsusai - ServerOptions: Configred to work like prometheus, load what is available,  save all ini options. -

--- src/Config/ServerOptions.pas
+++ src/Config/ServerOptions.pas
@@ -5,20 +5,42 @@ interface
 		IniFiles;
 
 	type
+		{Tsusai [05/06/06]: No existing ini file needed nor pre creation to read
+		writing to protected variables to set. I'm all for the prometheus way,
+		save after multiple changes.  the properties update the ini file in memory,
+		just need to do a update file afterwards to "set in stone"}
 		TServerOptions = class(TMemIniFile)
-				procedure Load;
-				procedure Save;
-				procedure WriteNewINI;
-			public
-				LoginPort : Word;
-				CharaPort : Word;
-				ZonePort : Word;
+		private
+			fLoginPort : Word;
+			fCharaPort : Word;
+			fZonePort : Word;
 
-				LANIp : String;
-				WANIp : String;
+			fWAN_IP : string;
+			fLAN_IP : string;
 
-				EnableMF : Boolean;
-			private
+			fEnableMF : Boolean;
+
+			procedure SetLoginPort(Value : word);
+			procedure SetCharaPort(Value : word);
+			procedure SetZonePort(Value : word);
+
+			procedure SetLAN_IP(Value : string);
+			procedure SetWAN_IP(Value : string);
+
+			procedure SetEnableMF(Value : boolean);
+
+		public
+			property LoginPort : Word read fLoginPort write SetLoginPort;
+			property CharaPort : Word read fCharaPort write SetCharaPort;
+			property ZonePort  : Word read fZonePort  write SetZonePort;
+
+			property LAN_IP : string read fLAN_IP write SetLAN_IP;
+			property WAN_IP : string read fWAN_IP write SetWAN_IP;
+
+			property EnableMF : boolean read fEnableMF write SetEnableMF;
+
+			procedure Load;
+			procedure Save;
 		end;
 
 implementation
@@ -28,41 +50,106 @@ implementation
 		Console;
 
 	procedure TServerOptions.Load;
-	begin
-		if NOT FileExists(FileName) then begin
-			WriteNewINI;
-		end;
-			LoginPort		:= StrToInt(ReadString('Communication','LoginPort','6900'));
-			CharaPort		:= StrToInt(ReadString('Communication','CharaPort','6121'));
-			ZonePort		:= StrToInt(ReadString('Communication','ZonePort','5121'));
-			WANIp				:= ReadString('Communication','WANIp','');
-			LANIp				:= ReadString('Communication','LANIp','127.0.0.1');
+	var
+		Section    : TStringList;
 
-			EnableMF := StrToBool(ReadString('LoginOptions','EnableMF','FALSE'));
+		procedure LoadCommunication;
+		begin
+			ReadSectionValues('Communication', Section);
+
+			fWAN_IP       := Section.Values['WAN_IP'];
+
+			if Section.Values['LAN_IP'] = '' then fLAN_IP := '127.0.0.1'
+			else fLAN_IP := Section.Values['LAN_IP'];
+
+			fLoginPort   := StrToIntDef(Section.Values['LoginPort'], 6900);
+			fCharaPort   := StrToIntDef(Section.Values['CharaPort'], 6121);
+			fZonePort    := StrToIntDef(Section.Values['ZonePort'], 5121);
+		end;
+
+		procedure LoadLoginOptions;
+		begin
+			ReadSectionValues('LoginOptions', Section);
+			fEnableMF    := StrToBoolDef(Section.Values['EnableMF'] ,false);
+		end;
+
+	begin
+		Section    := TStringList.Create;
+
+		Section.QuoteChar := '"';
+		Section.Delimiter := ',';
+
+		LoadCommunication;
+		LoadLoginOptions;
+
+		Section.Free;
 
 	end;
 
 	procedure TServerOptions.Save;
 	begin
-		WriteString('Communication','LoginPort',IntToStr(LoginPort));
-		WriteString('Communication','CharaPort',IntToStr(CharaPort));
-		WriteString('Communication','ZonePort',IntToStr(ZonePort));
-		WriteString('Communication','WANIp',WANIp);
-		WriteString('Communication','LANIp',LANIp);
+		WriteString('Communication', 'LoginPort', IntToStr(LoginPort));
+		WriteString('Communication', 'CharaPort', IntToStr(CharaPort));
+		WriteString('Communication', 'ZonePort',  IntToStr(ZonePort));
+		WriteString('Communication', 'WAN_IP',    WAN_IP);
+		WriteString('Communication', 'LAN_IP',    LAN_IP);
 
 		WriteString('LoginOptions','EnableMF',BoolToStr(EnableMF));
 		UpdateFile;
 	end;
 
-	procedure TServerOptions.WriteNewINI;
+	procedure TServerOptions.SetLoginPort(Value : word);
 	begin
-		WriteString('Communication','LoginPort','6900');
-		WriteString('Communication','CharaPort','6121');
-		WriteString('Communication','ZonePort','5121');
-		WriteString('Communication','WANIp','');
-		WriteString('Communication','LANIp','127.0.0.1');
-
-		WriteString('LoginOptions','EnableMF','FALSE');
-		UpdateFile;
+		if fLoginPort <> value then
+		begin
+			fLoginPort := value;
+			WriteString('Communication', 'LoginPort', IntToStr(LoginPort));
+		end;
 	end;
+
+	procedure TServerOptions.SetCharaPort(Value : word);
+	begin
+		if fCharaPort <> value then
+		begin
+			fCharaPort := value;
+			WriteString('Communication', 'CharaPort', IntToStr(CharaPort));
+		end;
+	end;
+
+	procedure TServerOptions.SetZonePort(Value : word);
+	begin
+		if fZonePort <> value then
+		begin
+			fZonePort := value;
+			WriteString('Communication', 'ZonePort', IntToStr(ZonePort));
+		end;
+	end;
+
+	procedure TServerOptions.SetLAN_IP(Value : string);
+	begin
+		if fLAN_IP <> value then
+		begin
+			fLAN_IP := value;
+			WriteString('Communication', 'LAN_IP', LAN_IP);
+		end;
+	end;
+
+	procedure TServerOptions.SetWAN_IP(Value : string);
+	begin
+		if fWAN_IP <> value then
+		begin
+			fWAN_IP := value;
+			WriteString('Communication', 'WAN_IP', WAN_IP);
+		end;
+	end;
+
+	procedure TServerOptions.SetEnableMF(Value : boolean);
+	begin
+		if fEnableMF <> value then
+		begin
+			fEnableMF := value;
+			WriteString('LoginOptions', 'EnableMF', BoolToStr(EnableMF));
+		end;
+	end;
+
 end.
