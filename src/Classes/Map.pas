7b53dfbe81f8c9a0c0273edfd24c1b6142254f35
RaX <onerax@gmail.com>
2006-11-02 14:38:59 +0000

    - Rev 44- [2006/11/01] - RaX - Map.pas - Implemented Tsusai's changed to GetPath which include, but  are not limited to, the following... 	GetPath changed to a boolean function with APath as a parameter passed by address. 	Addition of IsBlocked to check for passability versus a multi-line condition. [2006/11/01] - RaX - Map.pas - Added GetArea, a private method of TMap which creates a  search area based on our constant CHAR_CLICKAREA distance away from the starting point. [2006/11/02] - RaX - moved CHAR_CLICKAREA into Constants/GameConstants.pas. [2006/11/02] - RaX - Added GameConstants.pas. -

--- src/Classes/Map.pas
+++ src/Classes/Map.pas
@@ -13,48 +13,74 @@ interface
 
 uses
 	Types,
-  PointList;
+	PointList;
 
 type
-  //graph related types
-  TCell = record
-    Attribute : Byte;
-  end;
+	//graph related types
+	TCell = record
+    //what kind of tile is it
+		Attribute : Byte;
+		//Tsusai 11/09/06: Keep track of the number of things in the way, like icewall(s)
+		ObstructionCount : Byte;
+	end;
 
-  TGraph = array of array of TCell;
+	TGraph = array of array of TCell;
 
-  //flood types
+	//flood types
 	TFloodItem = record
 		Position : TPoint;
 		Path : array of TPoint;
-    PathLength : Integer;
+		PathLength : Integer;
 	end;
 
-  TFloodList = array of TFloodItem;
+	TFloodList = array of TFloodItem;
 
 //------------------------------------------------------------------------------
 //TMap                                                                  CLASS
 //------------------------------------------------------------------------------
-  TMap = class(TObject)
-  public
-    Graph : TGraph;
-  private
+TMap = class(TObject)
+	public
+		Graph : TGraph;
+	private
+		procedure GetArea(
+			const APoint	: TPoint;
+			var   AnArea	: TGraph;
+			var   XMod		: Integer;
+			var   YMod		: Integer
+		);
+	protected
 
-  protected
+	published
+		Constructor Create();
+		Destructor Destroy();override;
 
-  published
-    Constructor Create();
-    Destructor Destroy();override;
-    Function GetPath(StartPoint : TPoint; EndPoint : TPoint) : TPointList;
-  end;
+		Function GetPath(
+			StartPoint : TPoint;
+			EndPoint : TPoint;
+			var APath : TPointList
+		) : boolean;
+
+end;
 //------------------------------------------------------------------------------
 
 implementation
+uses
+  GameConstants;
+
+//checks to see if a cell is blocked.
+	function IsBlocked(
+		const AGraph : TGraph;
+		const APoint : TPoint
+	) : boolean; Forward;
 
 const
-  CHAR_CLICKAREA = 16;//the distance away a character can click from itself
-  //this array holds the possible directions from a point.
-  Directions : array[0..7] of TPoint = ((X:0;Y:1),(X:1;Y:1),(X:1;Y:0),(X:1;Y:-1),(X:0;Y:-1),(X:-1;Y:-1),(X:-1;Y:0),(X:-1;Y:1));
+	//this array holds the possible directions from a point.
+  //shouldn't be needed anywhere else, and shouldn't need to be changed.
+  //therefore, it's here instead of in GameConstants =)
+	Directions : array[0..7] of TPoint = (
+		(X:0;Y:1),(X:1;Y:1),(X:1;Y:0),(X:1;Y:-1),
+		(X:0;Y:-1),(X:-1;Y:-1),(X:-1;Y:0),(X:-1;Y:1)
+	);
 
 //------------------------------------------------------------------------------
 //TMap.Create()
@@ -67,7 +93,8 @@ const
 //------------------------------------------------------------------------------
 Constructor TMap.Create();
 begin
-  inherited;
+	inherited;
+	//add stuff here
 end;
 //------------------------------------------------------------------------------
 
@@ -82,12 +109,87 @@ end;
 //------------------------------------------------------------------------------
 Destructor TMap.Destroy();
 begin
-  inherited;
+	//add stuff here
+	inherited;
 end;
 //------------------------------------------------------------------------------
 
 //------------------------------------------------------------------------------
-//TMap.GetPath()
+//TMap.GetArea()                                                     PROCEDURE
+//------------------------------------------------------------------------------
+//  What it does -
+//      It fills the TGraph, AnArea, with the contents of TMap.Graph based on
+//		the distance defined by CHAR_CLICKAREA away from the character in the 4
+//		cardinal directions.
+//
+//  Changes -
+//    November 1st, 2006 - RaX - Created.
+//------------------------------------------------------------------------------
+procedure TMap.GetArea(const APoint : TPoint; var AnArea : TGraph; var XMod : Integer; var YMod : Integer);
+var
+	Index			: Integer;
+	XMax			: Integer;//last point in x axis on map
+	YMax			: Integer;//last point in y axis on map
+	XAreaMax	: Integer;//last x point in area
+	YAreaMax	: Integer;//last y point in area
+begin
+	XMax := Length(Graph)-1;
+	YMax := Length(Graph[0])-1;
+
+	//first, we get our areas position(really nasty condition follows)
+	//get XMod and XAreaMax
+	if (APoint.X-CHAR_CLICKAREA >= 0) AND (APoint.X+CHAR_CLICKAREA <= XMax) then
+	begin
+		XMod := APoint.X - CHAR_CLICKAREA;
+		XAreaMax := APoint.X + CHAR_CLICKAREA;
+	end else
+	if (APoint.X - CHAR_CLICKAREA < 0) AND (APoint.X + CHAR_CLICKAREA <= XMax) then
+	begin
+		XMod := 0;
+		XAreaMax := APoint.X + CHAR_CLICKAREA;
+	end else
+	if (APoint.X - CHAR_CLICKAREA >= 0) AND (APoint.X + CHAR_CLICKAREA > XMax) then
+	begin
+		XMod := APoint.X - CHAR_CLICKAREA;
+		XAreaMax := XMax;
+	end else
+	begin
+		XMod := 0;
+		XAreaMax := XMax;
+	end;
+
+	//Get YMod and YAreaMax
+		if (APoint.Y-CHAR_CLICKAREA >= 0) AND (APoint.Y+CHAR_CLICKAREA <= YMax) then
+	begin
+		YMod := APoint.Y - CHAR_CLICKAREA;
+		YAreaMax := APoint.Y + CHAR_CLICKAREA;
+	end else
+	if (APoint.Y - CHAR_CLICKAREA < 0) AND (APoint.Y + CHAR_CLICKAREA <= YMax) then
+	begin
+		YMod := 0;
+		YAreaMax := APoint.Y + CHAR_CLICKAREA;
+	end else
+	if (APoint.Y - CHAR_CLICKAREA >= 0) AND (APoint.Y + CHAR_CLICKAREA > YMax) then
+	begin
+		YMod := APoint.Y - CHAR_CLICKAREA;
+		YAreaMax := YMax;
+	end else
+	begin
+		YMod := 0;
+		YAreaMax := YMax;
+	end;
+
+	//finally, build our area
+	AnArea := copy(Graph,XMod,XAreaMax);
+	for Index := XMod to (XAreaMax) do
+	begin
+		AnArea[Index-XMod] := copy(Graph[Index], YMod, YAreaMax);
+	end;
+end;
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+//TMap.GetPath()                                                      FUNCTION
 //------------------------------------------------------------------------------
 //  What it does -
 //      This is a pathing algorithm that I like to call a "wave" algorithm
@@ -98,125 +200,141 @@ end;
 //
 //  Changes -
 //    October 30th, 2006 - RaX - Created.
+//    November 1st, 2006 - Tsusai - moved the check for passability into
+//     IsBlocked. Changed to a boolean function and our path is assigned via a
+//     TPointList parameter passed by address, APath.
 //------------------------------------------------------------------------------
-function TMap.GetPath(StartPoint : TPoint; EndPoint : TPoint) : TPointList;
+function TMap.GetPath(
+	StartPoint : TPoint;
+	EndPoint : TPoint;
+	var APath : TPointList
+) : boolean;
 var
-	Done		          : Boolean;
-  AnArea            : TGraph;
-	APath		          : TPointList;
-	AFloodList	      : TFloodList;
-  AFloodListLength  : Integer;
-	AFloodItem	      : TFloodItem;
-	NewFloodItem	    : TFloodItem;
-  APoint            : TPoint;
-	NewFloodList	    : TFloodList;
-  NewFloodListLength: Integer;
-	Index		          : Integer;
-	Directions	      : array of TPoint;
-	DirectionIndex    : Integer;
-  PossiblePos       : TPoint;
-  XMax              : Integer;//maximum y value in our area
-  YMax              : Integer;//maximum x value in our area
+	AFloodList				: TFloodList;
+	AFloodListLength	: Integer;
+  NewFloodList			: TFloodList;
+	NewFloodListLength: Integer;
+	AFloodItem				: TFloodItem;
+	NewFloodItem			: TFloodItem;
+	APoint						: TPoint;
+	Index							: Integer;
+	DirectionIndex		: Integer;
+	PossiblePosition  : TPoint;
+  AnArea						: TGraph;//our search area
+	XMod							: Integer;//x offset in AnArea from the point in TMap.Graph
+	YMod							: Integer;//y offset in AnArea from the point in TMap.Graph
+	XMax							: Integer;//maximum y value in our area
+	YMax							: Integer;//maximum x value in our area
 
 begin
-  //check to make sure we're wasting clock cycles for a reason...
-  //(checking to see if the endpoint is within range);
-  if ((StartPoint.X + CHAR_CLICKAREA) <= EndPoint.X) OR
-     ((StartPoint.X - CHAR_CLICKAREA) > EndPoint.X) OR
-     ((StartPoint.Y + CHAR_CLICKAREA) <= EndPoint.Y) OR
-     ((StartPoint.Y - CHAR_CLICKAREA) > EndPoint.Y) then
-  begin
-    Result := TPointList.Create;
-    Exit;
-  end;
+	Result := false;
+	//check to make sure we're wasting clock cycles for a reason...
+	//(checking to see if the endpoint is within range);
+	if ((StartPoint.X + CHAR_CLICKAREA) < EndPoint.X) OR
+		 ((StartPoint.X - CHAR_CLICKAREA) > EndPoint.X) OR
+		 ((StartPoint.Y + CHAR_CLICKAREA) < EndPoint.Y) OR
+		 ((StartPoint.Y - CHAR_CLICKAREA) > EndPoint.Y) then
+	begin
+		Exit;
+	end;
 
-  //initialize variables
-	Done		      := FALSE;
-  APath         := TPointList.Create;
 
-  //copy our map's graph
-  AnArea := copy(Graph);
-  for Index := 0 to Length(Graph) - 1 do
-  begin
-    AnArea[Index] := copy(Graph[Index]);
-  end;
-  XMax          := Length(AnArea);
-  YMax          := Length(AnArea[0]);
+	//Get Our Area and area constraints
+	GetArea(StartPoint, AnArea, XMod, YMod);
+	XMax          := Length(AnArea);
+	YMax          := Length(AnArea[0]);
 
-  //initialize our first flood item
+	//initialize our first flood item
 	AFloodItem.Position:= StartPoint;
-  AFloodItem.PathLength := 0;
+	AFloodItem.PathLength := 0;
 
-  //initialize our flood lists
-  AFloodListLength := 1;
-  SetLength(AFloodList, AFloodListLength);
-  AFloodList[AFloodListLength-1] := AFloodItem;
+	//initialize our flood lists
+	AFloodListLength := 1;
+	SetLength(AFloodList, AFloodListLength);
+	AFloodList[AFloodListLength-1] := AFloodItem;
 
 	//while we havn't found the end point...
-	while (AFloodListLength > 0) AND (NOT Done) do
+	while (AFloodListLength > 0) AND (NOT Result) do
 	begin
-    //reinitialize Index and NewFloodListLen for each AFloodList
-    Index := 0;
-    NewFloodListLength := 0;
-		while (Index < AFloodListLength) AND (NOT Done) do
+		//reinitialize Index and NewFloodListLen for each AFloodList
+		Index := 0;
+		NewFloodListLength := 0;
+		while (Index < AFloodListLength) AND (NOT Result) do
 		begin
 			//initialize our flooditem which we will propagate from.
 			AFloodItem  := AFloodList[Index];
 			//loop through each direction, create a new flood item for each.
-      DirectionIndex := 0;
-      while (DirectionIndex < 8) AND (NOT Done) do
-      begin
-        //get the expected position of our new flood item
-        PossiblePos.X := AFloodItem.Position.X + Directions[DirectionIndex].X;
-        PossiblePos.Y	:= AFloodItem.Position.Y + Directions[DirectionIndex].Y;
-        //check if coordinates are in range
-        if    (PossiblePos.X >= 0) AND (PossiblePos.X < XMax)
-          AND (PossiblePos.Y >= 0) AND (PossiblePos.Y < YMax)
-          AND (PossiblePos.X <= StartPoint.X+CHAR_CLICKAREA) AND (PossiblePos.X >= StartPoint.X-CHAR_CLICKAREA)
-          AND (PossiblePos.Y <= StartPoint.Y+CHAR_CLICKAREA) AND (PossiblePos.Y >= StartPoint.Y-CHAR_CLICKAREA) then
-        begin
-          //check if the square is passable, if it is...
-          if (AnArea[PossiblePos.X][PossiblePos.Y].Attribute <> 1) AND
-             (AnArea[PossiblePos.X][PossiblePos.Y].Attribute <> 5) then
-          begin
-            //build our flood item.
-            NewFloodItem.Position := PossiblePos;
-            NewFloodItem.Path := AFloodItem.Path;
-            NewFloodItem.PathLength := AFloodItem.PathLength;
-            APoint.X := NewFloodItem.Position.X;
-            APoint.Y := NewFloodItem.Position.Y;
-            inc(NewFloodItem.PathLength);
-            SetLength(NewFloodItem.Path, NewFloodItem.PathLength);
-            NewFloodItem.Path[NewFloodItem.PathLength-1] := APoint;
-            //add it to the NewFloodList
-            inc(NewFloodListLength);
-            SetLength(NewFloodList, NewFloodListLength);
-            NewFloodList[NewFloodListLength-1] := NewFloodItem;
-            //set it's position impassable in our area
-            AnArea[NewFloodItem.Position.X][NewFloodItem.Position.Y].Attribute := 1;
+			DirectionIndex := 0;
+			while (DirectionIndex < 8) AND (NOT Result) do
+			begin
+				//get the expected position of our new flood item
+				PossiblePosition.X := AFloodItem.Position.X + Directions[DirectionIndex].X;
+				PossiblePosition.Y	:= AFloodItem.Position.Y + Directions[DirectionIndex].Y;
+				//check if coordinates are in range
+				if    (PossiblePosition.X >= 0) AND (PossiblePosition.X < XMax)
+					AND (PossiblePosition.Y >= 0) AND (PossiblePosition.Y < YMax) then
+				begin
+					//check if the square is passable, if it is...
+					if not IsBlocked(AnArea,PossiblePosition) then
+					begin
+						//build our flood item.
+						NewFloodItem.Position := PossiblePosition;
+						NewFloodItem.Path := AFloodItem.Path;
+						NewFloodItem.PathLength := AFloodItem.PathLength;
+						APoint.X := NewFloodItem.Position.X;
+						APoint.Y := NewFloodItem.Position.Y;
+						inc(NewFloodItem.PathLength);
+						SetLength(NewFloodItem.Path, NewFloodItem.PathLength);
+						NewFloodItem.Path[NewFloodItem.PathLength-1] := APoint;
+						//add it to the NewFloodList
+						inc(NewFloodListLength);
+						SetLength(NewFloodList, NewFloodListLength);
+						NewFloodList[NewFloodListLength-1] := NewFloodItem;
+						//set it's position impassable in our area so we don't search it anymore
+						AnArea[NewFloodItem.Position.X][NewFloodItem.Position.Y].Attribute := 1;
 
-            //check to see if we've found the end point... if we have...
-            if (NewFloodItem.Position.X = EndPoint.X) AND (NewFloodItem.Position.Y = EndPoint.Y) then
-            begin
-              //congratulations, we've done it.
-              APath.Assign(NewFloodItem.Path);
-              Done  := TRUE;
-            end
-          end;
-        end;
-        //start propagating our next flood item in the next direction.
-        inc(DirectionIndex);
-      end;
-      //go to the next item in AFloodList
-      inc(Index);
-    end;
-    //swap our arrays around to propagate the next generation.
-    AFloodList := NewFloodList;
-    AFloodListLength := NewFloodListLength;
+						//check to see if we've found the end point... if we have...
+						if (NewFloodItem.Position.X = EndPoint.X) AND (NewFloodItem.Position.Y = EndPoint.Y) then
+						begin
+							//congratulations, we've done it.
+							APath.Assign(NewFloodItem.Path);
+							Result  := TRUE;
+							Exit;
+						end
+					end;
+				end;
+				//start propagating our next flood item in the next direction.
+				inc(DirectionIndex);
+			end;
+			//go to the next item in AFloodList
+			inc(Index);
+		end;
+		//swap our arrays around to propagate the next generation.
+		AFloodList := NewFloodList;
+		AFloodListLength := NewFloodListLength;
 	end;
-
-  //return our discovered path
-	Result := APath;
 end;
 //------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+//IsBlocked()                                                     FUNCTION
+//------------------------------------------------------------------------------
+//  What it does -
+//      It figures out if a specified cell in a TGraph is blocked.
+//
+//  Changes -
+//    November 1st, 2006 - Tsusai - Created.
+//------------------------------------------------------------------------------
+function IsBlocked(const AGraph : TGraph; const APoint : TPoint) : boolean;
+begin
+	//Assume it is not.
+	Result := false;
+	if (AGraph[APoint.X][APoint.Y].Attribute in [1,5]) OR
+		(AGraph[APoint.X][APoint.Y].ObstructionCount > 0 ) then
+	begin
+		Result := true
+	end;
+end;
+//------------------------------------------------------------------------------
+
 end.
---
+++ src/Constants/GameConstants.pas
@@ -0,0 +1,9 @@
+unit GameConstants;
+
+interface
+const
+	CHAR_CLICKAREA = 16;//the distance away a character can click from itself
+                      //CHAR_CLICKAREA used in Map.pas - Pathing.
+implementation
+
+end.
