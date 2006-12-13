1dfdeb071dccf453d99273e4ba91624cc23df340
Tsusai <tsusai@gmail.com>
2006-12-13 17:54:05 +0000

    - Rev 60- [2006/12/13] - Tsusai - Notes/Overview: JanSQL committed, its a TXT SQL system. [2006/12/13] - Tsusai - Added JanSQL folder and added into project. [2006/12/13] - Tsusai - Renamed SQL folder to MySQL. -

--- src/Common/Version.pas
+++ src/Common/Version.pas
@@ -1,7 +1,7 @@
 unit Version;
 interface
 
-var HeliosVersion   : String = 'Helios Ragnarok Server Version 0.0.0.59';
+var HeliosVersion   : String = 'Helios Ragnarok Server Version 0.0.0.60';
 
 implementation
 end.
