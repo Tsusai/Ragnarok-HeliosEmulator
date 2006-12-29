41052c05a43d967e3e6b0ab40edc5a6a985038bf
RaX <onerax@gmail.com>
2006-12-29 00:14:19 +0000

    - Rev 87 - [2006/12/28] - RaX - Added the 12/28 client to our packet_db. -

--- src/Common/Version.pas
+++ src/Common/Version.pas
@@ -11,7 +11,7 @@
 unit Version;
 interface
 
-var HeliosVersion   : String = 'Helios Ragnarok Server Version 0.0.0.86';
+var HeliosVersion   : String = 'Helios Ragnarok Server Version 0.0.0.87';
 
 implementation
 end.
