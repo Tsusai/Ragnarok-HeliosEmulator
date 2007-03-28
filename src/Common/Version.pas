78dc4ab6ff2be0dbe9e41ee2adffd96c5f5322df
ChrstphrR <chrstphrr@facebook.com>
2007-03-28 10:05:27 +0000

    - Rev 205 - [2007/03/28] CR - Overview: SVN Cleanup [2007/03/28] CR - Version: Updated version string. [2007/03/28] CR - SVN ignores set, to make wading through files when committing  less of a chore.  trunk/ and subfolders: svn:ignore tag set for "*.~*"  trunk/obj/: svn:ignore tag set for "*.dcu" -

--- src/Common/Version.pas
+++ src/Common/Version.pas
@@ -13,7 +13,7 @@
 unit Version;
 interface
 
-var HeliosVersion   : String = 'Helios Ragnarok Server Version 0.0.0.204';
+var HeliosVersion   : String = 'Helios Ragnarok Server Version 0.0.0.205';
 
 implementation
 end.
