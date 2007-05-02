d2e9bf5ac056b66a71d8556e3e5ca343d25dfca8
RaX <onerax@gmail.com>
2007-05-02 19:59:17 +0000

    - Rev 249 - [2007/05/02] - RaX - Fixed packet_db, this corrects the freezing problem experienced  in newer clients. (9/18+) -

--- src/Common/Version.pas
+++ src/Common/Version.pas
@@ -14,7 +14,7 @@ unit Version;
 interface
 
 var
-	HeliosVersion : String = 'Helios Ragnarok Server Version 0.0.0.248';
+	HeliosVersion : String = 'Helios Ragnarok Server Version 0.0.0.249';
 
 implementation
 end.
