988684c8917ed01a4ba9fed19db7e0ef8b277851
Tsusai <tsusai@gmail.com>
2007-05-31 19:23:51 +0000

    - Rev 274 - [2007/05/29] Tsusai - RELEASE 1. See Release Notes.txt. -

--- src/Common/Version.pas
+++ src/Common/Version.pas
@@ -14,7 +14,7 @@ unit Version;
 interface
 
 var
-	HeliosVersion : String = 'Helios Ragnarok Server Version 0.0.0.273';
+	HeliosVersion : String = 'Helios Ragnarok Server Version 0.0.1.274';
 
 implementation
 end.
