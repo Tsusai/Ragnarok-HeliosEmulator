00ce61866166534f0acda2ef25f3e64cb2d9a5e7
Spre <unknown@unknown.na>
2008-09-26 14:34:45 +0000

    - Rev 518 - [2008/09/25] Spre - Removed the development MD5 -

--- src/Common/Version.pas
+++ src/Common/Version.pas
@@ -19,7 +19,7 @@ unit Version;
 interface
 
 var
-	HeliosVersion : String = 'Helios Ragnarok Server Version 0.0.2.519';
+	HeliosVersion : String = 'Helios Ragnarok Server Version 0.0.2.520';
 
 implementation
 end.
