dc857239c113b3bb0cddde5f90a248fb908ff166
Aeomin <aeomin@facebook.com>
2008-10-04 18:04:58 +0000

    - Rev 574 - [2008/10/04] Aeomin - Updated packet db that can support 2008-10-01 client. -

--- src/Common/Version.pas
+++ src/Common/Version.pas
@@ -19,7 +19,7 @@ unit Version;
 interface
 
 var
-	HeliosVersion : String = 'Helios Ragnarok Server Version 0.0.2.573';
+	HeliosVersion : String = 'Helios Ragnarok Server Version 0.0.2.574';
 
 implementation
 end.
