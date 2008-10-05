63bbbf6dfb2708ad4af5a6198061d9e0f8dc771e
Aeomin <aeomin@facebook.com>
2008-10-05 02:53:44 +0000

    - Rev 578 - [2008/10/04] Aeomin - Another attempt to fix attack animation. -

--- src/Common/Version.pas
+++ src/Common/Version.pas
@@ -19,7 +19,7 @@ unit Version;
 interface
 
 var
-	HeliosVersion : String = 'Helios Ragnarok Server Version 0.0.2.577';
+	HeliosVersion : String = 'Helios Ragnarok Server Version 0.0.2.578';
 
 implementation
 end.
