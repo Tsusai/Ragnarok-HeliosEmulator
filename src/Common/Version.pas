097fd22d2953355dede4b1a7024d8713dc304cec
Tsusai <tsusai@gmail.com>
2008-11-18 19:14:07 +0000

    - Rev 632 - [2008/11/18] Tsusai - Notes/Overview: Removed Delphi "__history" temp from SVN -

--- src/Common/Version.pas
+++ src/Common/Version.pas
@@ -19,7 +19,7 @@ unit Version;
 interface
 
 var
-	HeliosVersion : String = 'Helios Ragnarok Server Version 0.0.2.631';
+	HeliosVersion : String = 'Helios Ragnarok Server Version 0.0.2.632';
 
 implementation
 end.
